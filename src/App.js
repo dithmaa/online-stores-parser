9ccdd77bc3bcf205c32c92b983a34bb5d789378f
Baltukov Nomto <nomto_04@mail.ru>
2022-04-09 13:32:29 +0800

    Мой первый коммит

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+
+function App({name = "Alpen Gold", price = 3000, image="https://irecommend.ru/sites/default/files/product-images/1064357/5YwJG6L9XSIFR0uS7ZkXQ.jpg", storeName = "Абсолют"}) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <div className="cards d-flex flex-wrap">
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+          <div className="card">
+            <div className="cardStoreName">
+              <h2>{storeName}</h2>
+            </div>
+            <div className="cardImage">
+              <img src={image} alt="" />
+            </div>
+            <div className="cardName"><h3>{name}</h3></div>
+            <div className="cardPrice">{price} р</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
